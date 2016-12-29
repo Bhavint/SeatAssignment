abc76b75b7c8ae88aa7b96110bde8bdf98a88264
Bhavin Thakkar <bthakkar@tavisca.com>
2016-12-29 13:16:58 -0700

    Add skeleton for solution

---
+++ SeatAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeatAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*TODO
+             * 1. Read Input
+             * 2. Assign Seats
+             * 3. Return Results
+            */
+        }
+    }
+}

7dc81f5114d7b6268d4d4d6e75cf9d4e2eec5c98
Bhavin Thakkar <bthakkar@tavisca.com>
2017-01-09 02:41:04 -0700

    Add Tests. Add Code Coverage Support

--- SeatAssignment.Tests/SeatAssignmentTests.cs
+++ SeatAssignment.Tests/SeatAssignmentTests.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SeatAssignment.BusinessLogic;
-using System.Collections.Generic;
 using System.Linq;
-using SeatAssignment.Entities;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using SeatAssignment.Interfaces;
 
 namespace SeatAssignment.Tests
 {
     [TestClass]
     public class SeatAssignmentTests
     {
+        public static IUnityContainer container;
+
         private TestContext testContextInstance;
 
         public TestContext TestContext
@@ -25,7 +26,12 @@ namespace SeatAssignment.Tests
                 testContextInstance = value;
             }
         }
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            container = new UnityContainer().LoadConfiguration();
 
+        }
         [TestMethod]
         public void TestFullCapacity()
         {
@@ -45,6 +51,88 @@ namespace SeatAssignment.Tests
             var requestedSeats = requests.Sum(x => x.NumberOfSeats);
             Assert.IsTrue(assignedSeats.Count == requestedSeats);
             Assert.IsTrue(assignedSeats.Distinct().Count() == requestedSeats);
+
+        }
+
+        [TestMethod]
+        public void TestAllocationEfficiency()
+        {
+            var inputReader = new FileInputReader("InputFiles\\AllocationEfficiencyTest.txt");
+            var requests = inputReader.GetTicketRequests();
+
+            var theaterManager = container.Resolve<ITheaterManager>("internalTheaterManager");
+
+            Assert.IsInstanceOfType(theaterManager, typeof(FairTheaterManager));
+
+            var assignments = theaterManager.AssignSeats(requests);
+
+            Assert.IsTrue(assignments[0].AssignedSeats.Count(seat => seat.Contains("A")) == assignments[0].AssignedSeats.Count);
+            Assert.IsTrue(assignments[1].AssignedSeats.Count(seat => seat.Contains("B")) == assignments[1].AssignedSeats.Count);
+            Assert.IsTrue(assignments[2].AssignedSeats.Count(seat => seat.Contains("B")) == assignments[2].AssignedSeats.Count);
+            Assert.IsTrue(assignments[3].AssignedSeats.Count(seat => seat.Contains("C")) == assignments[3].AssignedSeats.Count);
+            Assert.IsTrue(assignments[4].AssignedSeats.Count(seat => seat.Contains("C")) == assignments[4].AssignedSeats.Count);
+            Assert.IsTrue(assignments[5].AssignedSeats.Count(seat => seat.Contains("A")) == assignments[5].AssignedSeats.Count);
+        }
+
+        [TestMethod]
+        public void TestLargeGroupFavoringTheaterManager()
+        {
+            var inputReader = new FileInputReader("InputFiles\\AllocationEfficiencyTest.txt");
+            var requests = inputReader.GetTicketRequests();
+
+            var theaterManager = container.Resolve<ITheaterManager>("largeGroupFavoringTheaterManager");
+
+            Assert.IsInstanceOfType(theaterManager, typeof(LargeGroupFavoringTheaterManager));
+
+            var assignments = theaterManager.AssignSeats(requests);
+
+            Assert.IsTrue(assignments[0].AssignedSeats.Count(seat => seat.Contains("C")) == assignments[0].AssignedSeats.Count);
+            Assert.IsTrue(assignments[1].AssignedSeats.Count(seat => seat.Contains("B")) == assignments[1].AssignedSeats.Count);
+            Assert.IsTrue(assignments[2].AssignedSeats.Count(seat => seat.Contains("B")) == assignments[2].AssignedSeats.Count);
+            Assert.IsTrue(assignments[3].AssignedSeats.Count(seat => seat.Contains("A")) == assignments[3].AssignedSeats.Count);
+            Assert.IsTrue(assignments[4].AssignedSeats.Count(seat => seat.Contains("A")) == assignments[4].AssignedSeats.Count);
+            Assert.IsTrue(assignments[5].AssignedSeats.Count(seat => seat.Contains("C")) == assignments[5].AssignedSeats.Count);
+        }
+
+        [TestMethod]
+        public void TestSmallGroupFavoringTheaterManager()
+        {
+            var inputReader = new FileInputReader("InputFiles\\AllocationEfficiencyTest.txt");
+            var requests = inputReader.GetTicketRequests();
+
+            var theaterManager = container.Resolve<ITheaterManager>("smallGroupFavoringTheaterManager");
+
+            Assert.IsInstanceOfType(theaterManager, typeof(SmallGroupFavoringTheaterManager));
+
+            var assignments = theaterManager.AssignSeats(requests);
+
+            Assert.IsTrue(assignments[0].AssignedSeats.Count(seat => seat.Contains("B")) == assignments[0].AssignedSeats.Count);
+            Assert.IsTrue(assignments[1].AssignedSeats.Count(seat => seat.Contains("C")) == assignments[1].AssignedSeats.Count);
+            Assert.IsTrue(assignments[2].AssignedSeats.Count(seat => seat.Contains("A")) == assignments[2].AssignedSeats.Count);
+            Assert.IsTrue(assignments[3].AssignedSeats.Count(seat => seat.Contains("D")) == assignments[3].AssignedSeats.Count);
+            Assert.IsTrue(assignments[4].AssignedSeats.Count(seat => seat.Contains("A")) == assignments[4].AssignedSeats.Count);
+            Assert.IsTrue(assignments[5].AssignedSeats.Count(seat => seat.Contains("A")) == assignments[5].AssignedSeats.Count);
+        }
+
+        [TestMethod]
+        public void TestSimpleTheaterManager()
+        {
+            var inputReader = new FileInputReader("InputFiles\\SimpleAllocationInput.txt");
+            var requests = inputReader.GetTicketRequests();
+
+            var theaterManager = new SimpleTheaterManager();
+
+            Assert.IsInstanceOfType(theaterManager, typeof(SimpleTheaterManager));
+            var assignments = theaterManager.AssignSeats(requests);
+
+            Assert.IsTrue(assignments[0].AssignedSeats.Count(seat => seat.Contains("A")) == 10);
+            Assert.IsTrue(assignments[1].AssignedSeats.Count(seat => seat.Contains("A")) == 10);
+            Assert.IsTrue(assignments[1].AssignedSeats.Count(seat => seat.Contains("B")) == 10);
+            Assert.IsTrue(assignments[2].AssignedSeats.Count(seat => seat.Contains("B")) == 10);
+            Assert.IsTrue(assignments[3].AssignedSeats.Count(seat => seat.Contains("C")) == 5);
+            Assert.IsTrue(assignments[4].AssignedSeats.Count(seat => seat.Contains("C")) == 5);
+            Assert.IsTrue(assignments[5].AssignedSeats.Count(seat => seat.Contains("C")) == 10);
+            Assert.IsTrue(assignments[5].AssignedSeats.Count(seat => seat.Contains("D")) == 5);
 
         }
     }
